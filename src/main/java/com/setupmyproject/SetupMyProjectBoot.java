2a67da2c907783f0fb53fb18c3ec155759d84425
asouza <alots.ssa@gmail.com>
2017-05-10 21:22:12 -0300

    atualizando boot

--- src/main/java/com/setupmyproject/SetupMyProjectBoot.java
+++ src/main/java/com/setupmyproject/SetupMyProjectBoot.java
@@ -2,8 +2,8 @@ package com.setupmyproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
 @SpringBootApplication
